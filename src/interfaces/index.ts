8f5d6c564bb6fd423e5d132195695c1e44c0c11c
rodolphonikqg <rodolpho.oliveira@acct.global>
2024-08-10 20:32:23 -0300

    feat: add TypeScript interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface ChatItem {
+  name: string
+  avatarUrl: string
+  message: string
+  groupMessager?: string
+  isRead: boolean
+  isGroup: boolean
+  isMuted: boolean
+  lastInteraction: string
+}
+
+export interface ChatStore {
+  items: ChatItem[]
+  fetchItems: () => void
+}
