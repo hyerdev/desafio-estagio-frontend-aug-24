7d81e314d93b4b1112180f5e46b1814ca350abd6
rodolphonikqg <rodolpho.oliveira@acct.global>
2024-08-10 10:34:54 -0300

    feat: created two components for rendering svgs

---
+++ src/components/DownArrowIcon.tsx
@@ -0,0 +1,27 @@
+const DownArrowIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18px"
+      height="18px"
+      viewBox="-102.4 -102.4 1228.80 1228.80"
+      version="1.1"
+      fill="#8696A0"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"
+          fill="#8696A0"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default DownArrowIcon
