5a781c34dce67de635a2e22760e312046ab689bf
rodolphonikqg <rodolpho.oliveira@acct.global>
2024-08-11 14:17:21 -0300

    style: final layout adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ChatList from './components/ChatList'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-t from-[#150d21] to-[#180f27] w-full flex items-center justify-center">
+    <div className="min-h-screen h-screen bg-gradient-to-t from-[#150d21] to-[#180f27] w-full flex items-center justify-center">
       <ChatList />
     </div>
   )
--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -14,12 +14,14 @@ function ChatList() {
   const filteredItems = filterItems(items, searchContent, filter)
 
   return (
-    <div className="flex flex-col max-w-full bg-dark-black">
+    <div className="flex flex-col h-full max-w-full bg-dark-black sm:w-[500px]">
       <h1 className='h-[60px] px-6 flex items-center font-bold text-2xl text-light-gray'>Chats</h1>
       <ChatListSearch />
-      {filteredItems.map((item, index) => (
-        <ChatListItem key={index} item={item} />
-      ))}
+      <div className='overflow-y-auto scrollbar w-full'>
+        {filteredItems.map((item, index) => (
+          <ChatListItem key={index} item={item} />
+        ))}
+      </div>
     </div>
   )
 }
