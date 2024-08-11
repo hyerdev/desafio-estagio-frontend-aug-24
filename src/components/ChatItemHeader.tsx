40ba281db53dfa104f746aa866b608264c76b096
rodolphonikqg <rodolpho.oliveira@acct.global>
2024-08-11 13:43:29 -0300

    feat: add date formatting for chat items

--- src/components/ChatItemHeader.tsx
+++ src/components/ChatItemHeader.tsx
@@ -1,9 +1,12 @@
 import { ChatItemHeaderProps } from '@/interfaces'
+import { formatDate } from '@/utils/formatDate'
 
 const ChatItemHeader = ({ name, lastInteraction }: ChatItemHeaderProps) => (
   <div className="flex items-center justify-between">
     <p className="text-light-gray">{name}</p>
-    <span className="text-xs text-normal-gray">{lastInteraction}</span>
+    <span className="text-xs text-normal-gray">
+      {formatDate(lastInteraction)}
+    </span>
   </div>
 )
 
